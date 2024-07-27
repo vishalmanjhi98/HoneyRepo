b46a802b17e6b4e11f2c73220bdea0c29c887a5f
Vishal Manjhi <vmanjhi98@gmail.com>
2024-07-28 01:16:37 +0530

    all files upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    static int test(int a,int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 9;
+
+        int b = 5;
+
+        int res = test(a,b);
+    }
+}
